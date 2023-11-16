dcfe8d399e78e278f60c15cd152dfa5d9180afd0
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-11-16 14:41:33 -0500

    started my nav and header jsx, also started inputing info into my pages

---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>
+        Hi my name is Joseph Oñate I'm 34 years old I'm originally from California.<br />
+        Now live in central Florida happily married with two crazy kids. <br />
+        I was in the Air Force as an active duty member for 10 years and now I'm in the reserves.<br /> 
+        I'm currently a Project Engineer for View inc. <br />
+        I went to school for Electronic Engineer I have my AAS and I want to learn more about coding <br />
+        so that I can progress in my job and future endevours.
+        </p>
+      </div>
+    );
+  }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Oñate</h1>
+    </header>
+  );
+}
+
+export default Header;
