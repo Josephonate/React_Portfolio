4890205e8ddef2916fb2b829e8daa42c4f50df45
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-11-20 23:37:01 -0500

    replaced form on contact to just contact info

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,57 +1,25 @@
-import { useState } from 'react';
+import Card from "react-bootstrap/Card";
 
-
-function Form() {
-  // Here we set two state variables for firstName and lastName using `useState`
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = e.target;
-
-    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-    return name === 'firstName' ? setFirstName(value) : setLastName(value);
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // Alert the user their first and last name, clear the inputs
-    alert(`Hello ${firstName} ${lastName}`);
-    setFirstName('');
-    setLastName('');
-  };
-
+export default function Contact() {
   return (
-    <div className="container text-center">
-      <h1>
-        Hello {firstName} {lastName}
-      </h1>
-      <form className="form" onSubmit={handleFormSubmit}>
-        <input
-          value={firstName}
-          name="firstName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="First Name"
-        />
-        <input
-          value={lastName}
-          name="lastName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Last Name"
-        />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
+    <div
+      style={{
+        
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >
+      <Card style={{ backgroundColor: "#EAE1DF",width: "60rem" }}>
+        <Card.Body>
+          <Card.Title>Contact Me</Card.Title>
+          <Card.Text style={{ fontSize: "30px" }}>
+           <a href="mailto:josephonate@yahoo.com">josephonate@yahoo.com</a>
+           <p>My Cell is (951) 816-8897</p>
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
-
-export default Form;
-
-  
